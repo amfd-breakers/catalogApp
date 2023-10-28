8361fb9844106db025288da479becfbeb682778d
amfd-breakers <125905646+amfd-breakers@users.noreply.github.com>
2023-10-28 19:34:31 +0500

    added dependencie(get)

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:catalog_app/core/home/home_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomeView(),
+    );
+  }
+}
